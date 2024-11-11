ccca6dcb199fd161599d34b9ef04d4d93c0a68c5
TelaTela <40137857+dzinnn@users.noreply.github.com>
2024-11-11 20:47:45 +0700

    increase supervision level description padding

--- public/build/assets/SupervisionIndicatorBadge-kQZ2v59Z.js
+++
@@ -1,6 +0,0 @@
-import{j as r}from"./app-CGyVb8Is.js";import{B as a}from"./badge-DeapLxbP.js";import{C as n}from"./check-CCoGvReR.js";import{c as s}from"./createLucideIcon-BQUYwqWN.js";import"./index-Bb4qSo10.js";import"./utils-BM_CldAA.js";/**
- * @license lucide-react v0.447.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const c=s("X",[["path",{d:"M18 6 6 18",key:"1bl5f8"}],["path",{d:"m6 6 12 12",key:"d8bk6v"}]]);function h({fulfilled:e=!1}){const t=e?"w-10 h-10 border-transparent bg-green-100 text-green-800 hover:bg-green-300":"w-10 h-10 border-transparent bg-red-100 text-red-800 hover:bg-red-300",o=e?r.jsx(n,{}):r.jsx(c,{});return r.jsx(a,{className:t,children:o})}export{h as default};
